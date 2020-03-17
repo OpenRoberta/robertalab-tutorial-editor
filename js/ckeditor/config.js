fc1a86d8c64dd753c71d526923f3c8889f76d08f
pmaurer <pmaurer@mp-mobil-32.iais.fraunhofer.de>
2020-03-17 15:52:08 +0100

    Update to new ckeditor Version. BREAKING FOR VIDEOS!

--- js/ckeditor/config.js
+++ js/ckeditor/config.js
@@ -1,36 +1,39 @@
 /**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+    // Define changes to default configuration here.
+    // For complete reference see:
+    // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
-	// The toolbar groups arrangement, optimized for a single toolbar row.
-	config.toolbarGroups = [
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'forms' },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'tools' },
-		{ name: 'others' },
-		{ name: 'about' }
-	];
+    // The toolbar groups arrangement, optimized for two toolbar rows.
+    config.toolbar = [
+        { name: 'document', items: [ 'Source'] },
+        { name: 'clipboard', items: [  'Undo', 'Redo', '-', 'PasteText', 'PasteFromWord'] },
+        { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
+        '/',
+        { name: 'styles', items: [ 'Format'] },
+        { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+        '/',
+        { name: 'styles', items: ['FontSize' ] },
+        { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+        { name: 'links', items: [ 'Link', 'Unlink'] },
+        { name: 'insert', items: [ 'base64image', 'Html5video', 'Table', 'HorizontalRule'] },
+        { name: 'about', items: [ 'About' ] },
+        { name: 'others', items: [ 'Others' ] }
+    ];
 
-	// The default plugins included in the basic setup define some buttons that
-	// are not needed in a basic editor. They are removed here.
-	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Strike,Subscript';
+    // Set the most common block elements.
+    config.format_tags = 'p;h1;h2;h3;pre';
 
-	// Dialog windows are also simplified.
-	config.removeDialogTabs = 'link:advanced';
-	
-	config.height = 320;
+    // Simplify the dialog windows.
+    config.removeDialogTabs = 'image:advanced;link:advanced';
+    
+    config.language = 'de';
+    config.uiColor = '#EEEEEE';
+    config.height = 320;
+    
+    config.readOnly = false;
 };
